8d3750bf8811eac23b34b4578b8e94dd6f5a394b
zLilo <murilofm.social@gmail.com>
2024-11-23 17:27:00 -0300

    Mutator para corrigir Enum

--- app/Models/Alunos.php
+++ app/Models/Alunos.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Facades\Log;
 use App\Enums\NivelEnsino;
 
 class Alunos extends Model
@@ -20,6 +22,26 @@ class Alunos extends Model
     protected $casts = [
         'nivel_ensino' => NivelEnsino::class,
     ];
+
+    public function setnivelEnsinoAttribute($value)
+    {
+        $cases = [];
+        foreach(NivelEnsino::cases() as $case){
+            $cases[] = $case->name;
+        }
+
+        if(in_array($value, $cases, false)){
+            switch($value){
+                case 'F1': $this->attributes['nivel_ensino'] = NivelEnsino::from('Fundamental 1')->value;
+                break;
+                case 'F2': $this->attributes['nivel_ensino'] = NivelEnsino::from('Fundamental 2')->value;
+                break;
+                case 'EM': $this->attributes['nivel_ensino'] = NivelEnsino::from('Ensino Médio')->value;
+                break;
+
+            }
+        }
+    }
 
     public function notas()
     {
