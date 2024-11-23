69e5247bdc02225bc62f36d07d3d5ccf3d0ce76c
zLilo <murilofm.social@gmail.com>
2024-11-23 17:36:22 -0300

    adicona metodo para rota post /alunos

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -5,60 +5,57 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Alunos;
+use Illuminate\Validation\Rule;
+use App\Enums\NivelEnsino;
 
 class AlunosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    private function matriculaGen($nome){
+        $nomesArr = explode(' ', $nome);
+        $iniciais = "";
+        foreach($nomesArr as $nome){
+            $iniciais .= strtoupper($nome[0]);
+        }
+        return $iniciais . mt_rand(10000, 99999);
+    }
+
     public function index()
     {
         return ['success' => 'Alunos listados com sucesso!', 'alunos' => Alunos::all()];
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nome' => 'required|string|max:255',
+            'nivel_ensino' => 'required|string|max:20',
+            'ano' => 'required|string|max:2'
+        ]);
+        $aluno = Alunos::create(array_merge($validated, ['matricula' => $this->matriculaGen($request->nome)]));
+
+        return ['success' => 'Alunos adicionado com sucesso!', 'aluno' => $aluno->only(['nome', 'matricula', 'ano', 'nivel_ensino'])];
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
